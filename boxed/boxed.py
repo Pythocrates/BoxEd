bad4416070f2b2d365951b7c1d739b224f6eed0b
Pythocrates <23015037+Pythocrates@users.noreply.github.com>
2020-09-08 19:21:02 +0200

    Add first empty test.

--- boxed/boxed.py
+++ boxed/boxed.py
@@ -1,6 +1,11 @@
 ''' This module contains the main base class.
 '''
 
+from contextlib import contextmanager
+
 
 class BoxEd:
-    pass
+    @contextmanager
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        yield None
---
+++ test/test_boxed.py
@@ -0,0 +1,9 @@
+''' This tests the BoxEd class.
+'''
+
+from boxed.boxed import BoxEd
+
+
+class TestBoxEd:
+    with BoxEd() as boxed:
+        pass
